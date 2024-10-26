1121e360e7301434f75e8c1d2d3701911e8fd5af
FrancescoBenassi <francybenna77@gmail.com>
2024-10-26 16:37:49 +0200

    feat js: added variables with DOM elements

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// variabili con all'interno elementi del DOM
+
+const calculatedPrice = document.getElementById('calculated-price');
+
+const nameUser = document.getElementById('name-user');
+
+const surnameUser = document.getElementById('surname-user');
+
+const emailUser = document.getElementById('email-user');
+
+const selectWork = document.getElementById('select-work');
+
+const textAreaUser = document.getElementById('textarea-user');
+
+const selectCoupon = document.getElementById('discount-coupon');
+
+const privacyRules = document.getElementById('privacy-rules');
+
+const resultPrice = document.getElementById('result');
